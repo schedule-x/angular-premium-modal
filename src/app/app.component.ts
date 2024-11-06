52da3aa9065f4db4962e8031f0620506fd753267
Tom Österlund <tom_jonathan.oesterlund@prosiebensat1.com>
2024-11-06 14:38:55 +0100

    set up schedule-x with premium modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarComponent } from "@schedule-x/angular";
+import { createCalendar, createViewWeek } from "@schedule-x/calendar";
+import {createInteractiveEventModal} from "@sx-premium/interactive-event-modal";
+import {createEventsServicePlugin} from "@schedule-x/events-service"; // can alternatively be added in your angular.json
+import '@schedule-x/theme-default/dist/index.css'
+import '@sx-premium/interactive-event-modal/index.css'
+
+const eventsService = createEventsServicePlugin();
+
+const eventModal = createInteractiveEventModal({
+  // dependency needed to add events
+  eventsService,
+
+  // (Optional): callback for when an event is added
+  onAddEvent: (event) => {
+    console.log(event)
+  },
+
+  // (Optional): callback for when an event is updated
+  onDeleteEvent: (eventId) => {
+    console.log(eventId)
+  },
+
+  // (Optional): configuration to only show title and description fields
+  fields: {
+    title: {},
+    description: {},
+  },
+})
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalendarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-premium-modal';
+  calendarApp = createCalendar({
+    events: [
+      {
+        id: '1',
+        title: 'Event 1',
+        start: '2024-11-08 03:00',
+        end: '2024-11-08 05:00',
+      },
+    ],
+
+    views: [createViewWeek()],
+
+    plugins: [
+      eventsService,
+      eventModal,
+    ],
+
+    callbacks: {
+      onDoubleClickDateTime: (dateTime) => {
+        eventModal.clickToCreate(dateTime);
+      }
+    },
+  });
 }
